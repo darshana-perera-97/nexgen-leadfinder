ba52b1708688221b6c7b2172dcc77fdb4d49d45c
Darshana Perera <ds.perera.1997@gmail.com>
2026-02-09 08:52:18 +0530

    updated frontend added

--- frontend/src/config/api.js
+++ frontend/src/config/api.js
@@ -1,6 +1,6 @@
 // ⚡ CHANGE THIS LINE TO SET YOUR BACKEND URL ⚡
-export const API_BASE_URL = process.env.REACT_APP_API_URL || 'http://localhost:5254/api'; // Change '/api' to 'http://localhost:3060/api' for development
-// export const API_BASE_URL = process.env.REACT_APP_API_URL || '/api'; 
+// export const API_BASE_URL = process.env.REACT_APP_API_URL || 'http://localhost:5254/api'; // Change '/api' to 'http://localhost:3060/api' for development
+export const API_BASE_URL = process.env.REACT_APP_API_URL || '/api'; 
 
 // API Endpoints
 export const API_ENDPOINTS = {
